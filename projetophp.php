e35c79aa74e20e74d44bc6957f5ea6aa47367d73
smklua1552 <samuel1552davi@gmail.com>
2025-03-25 22:20:40 -0300

    Add files via upload

---
+++ projetophp.php
@@ -0,0 +1,33 @@
+<?php
+$x=4;
+$y=4;
+
+$z=$x+$y;
+ 
+ if  ($z > 20){
+
+$a=$z+8;
+
+echo "O resultado é  $a ";
+ }
+
+
+
+
+
+
+elseif ($z<=20) {
+ 
+$b=$z-5;
+echo "o resultado é $z";
+}
+
+
+
+
+else {
+ echo "O resultado da soma é $z";
+}
+
+
+?>
